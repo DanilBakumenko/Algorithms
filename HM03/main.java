a31bfd24c2e2ffd2fed94aa52312dfffeda31b13
Danil <d.bakumenko@mail.ru>
2023-07-11 18:05:12 +0300

    Home Work 03

---
+++ HM03/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add(0);
+        list.add(2);
+        list.add(0);
+        list.add(3);
+        list.add(4);
+        list.add(9);
+
+        list.print();
+        list.reversePrint();
+    }
+}
